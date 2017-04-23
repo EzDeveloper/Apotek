80fda9575f2c3e5325f6dbe232413d8f05c9441d
Matthew Ariel <matthew_ariel_wangsit@yahoo.com>
2017-04-23 12:12:09 +0700

    Added user and token

--- app/Http/Controllers/RoleController.js
+++ app/Http/Controllers/RoleController.js
@@ -1,7 +1,70 @@
 'use strict'
 
+const Role = user('App/Model/Role')
+const Validator = use('Validator')
+
 class RoleController {
 
+	* index(request, response){
+		 const roles = yield Role.all()
+		 yield response.sendView('role/index', {roles:roles.toJSON()})
+	}
+
+	* create(request, response){
+		yield response.sendView('role/create')
+	}
+
+	* store(request, response){
+		const roleData = request.except('_csrf','submit')
+		const validation = yield Validator.validation(roleData, role.rules)
+		if (validation.fails()){
+			yield request
+				.withOnly('name') 
+				.andWith({ errors:validation.messages()})
+				.flash()
+			response.redirect('role/create')
+			return
+		}
+		yield role.create(roleData)
+		yield response.sendView('role/create', {successMessage: 'Created role Successfully'})
+
+	}
+
+	* show(request,response){
+		const role = yield Role.findBy('id',request.paran('id)'))
+		yield response.sendView('role/show',{role:role.toJSON()})
+	}
+
+	* edit(request,response){
+		const role = yield Role.findBy('id',request.param('id'))
+		yield response.sendView('role/edit', {role:role.toJSON()})
+	}
+
+	* update(request,response){
+		const roleId = request.param('id')
+		const roleData = request.except('_csrf','submit')
+		const validation = yield Validator.validate(roleData, role.updateRules(roleId))
+	
+		if(validation.fails()){
+			yield request
+			.withOnly('name','birth_date','address','phone','kis')
+			.andWith({errors:validation.messages()})
+			.flash()
+			response.redirect(roleId+'/edit')
+			return
+		}
+
+		const role = yield Role.findBy('id', roleId)
+		role.name = roleData.name
+		yield role.save()
+		yield response.redirect(roleId)
+	}
+
+	* destroy(request,response){
+		const role = yield Role.findBy('id',request.param('id'))
+		yield role.delete()
+		yield response.redirect('/role')
+	}
 }
 
 module.exports = RoleController
