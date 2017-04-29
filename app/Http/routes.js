5c67b0464aa88fd4d8c35ac1c5ec4cbd94c90ffb
Matthew Ariel <matthew_ariel_wangsit@yahoo.com>
2017-04-29 15:25:36 +0700

    fix add amount

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -22,7 +22,7 @@ Route.on('/').render('login')
 Route.resource('customer','CustomerController')
 Route.resource('user','UserController')
 Route.resource('role','RoleController')
-Route.get('stock/:id/add','StockController.add')
-Route.post('stock/:id/add','StockController.increase')
+Route.get('stock/add','StockController.add')
+Route.post('stock/add','StockController.increase')
 
 Route.on('/admin').render('base');
